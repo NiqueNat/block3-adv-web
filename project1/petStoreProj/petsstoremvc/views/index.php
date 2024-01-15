392be5540eb3b302ce40177980aa6ccee0452047
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2024-01-15 10:20:33 -0500

    Updated file

--- project1/petStoreProj/petsstoremvc/views/index.php
+++ project1/petStoreProj/petsstoremvc/views/index.php
@@ -3,19 +3,21 @@
 include_once __DIR__ . '/../controllers/config.php';
 include_once __DIR__ . '/../models/baseModel.php';
 include_once __DIR__ . '/../controllers/controller.php'; 
+
 $controller = new controller($pdo); 
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$pets = $controller->getPets(); 
+$pets = $controller->getPets(); // Fetch pet data
 
 foreach ($pets as $pet) {
-    echo "<p>{$pet->name}</p>";
-
+    $name = $pet->name ?? "Unknown";
+    echo "<p>{$name}</p>";
     var_dump($pet);
 }
+
 ?>
 
 <!DOCTYPE html>
